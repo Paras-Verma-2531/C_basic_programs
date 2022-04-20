0dae88b26260c62da7c02d2f32d0898e34c4504e
paras verma <parasverma2531@gmail.com>
2022-04-20 15:59:32 +0530

    Delete project_and_all directory

--- project_and_all/pythonstring.py
+++
@@ -1,8 +0,0 @@
-st=input("enter your string \n")
-ch=input("enter the character whose occurence u want to find..\n")
-c=0;
-for i in range(len(st)):
-    if(ch == st[i]):
-        c+=1;
-print(f" the character {ch} is present {c} times")    
-            
